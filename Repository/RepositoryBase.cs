f844fef06c5eaa383226d25973025f55cf8389ad
sharan <sharan.reddy@insight.com>
2020-12-02 18:12:51 +0800

    Add project files.

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            return !trackChanges ?
+                RepositoryContext.Set<T>()
+                .AsNoTracking() :
+                RepositoryContext.Set<T>();
+        }
+    }
+}
